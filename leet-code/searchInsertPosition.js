70731305e6ff81a6c199c5f29579b95f5a8f0631
beshoy <said.beshoy@yahoo.com>
2024-01-23 11:22:20 +0200

    search-problem

---
+++ leet-code/searchInsertPosition.js
@@ -0,0 +1,9 @@
+var searchInsert = function (nums, target) {
+  if (nums.includes(target)) {
+    return nums.indexOf(target);
+  } else {
+    nums.push(target);
+    nums.sort((a, b) => a - b);
+    return nums.indexOf(target);
+  }
+};
