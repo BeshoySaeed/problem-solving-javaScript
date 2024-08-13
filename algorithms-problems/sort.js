b54446c51afc155da0a004caa39b7973475a3812
Bishoy <Bishoy@gmail.com>
2024-08-13 15:48:01 +0300

    selection-insertion-Sort

--- algorithms-problems/sort.js
+++ algorithms-problems/sort.js
@@ -21,4 +21,48 @@ Array.prototype.bubbleNumber = function () {
   return array;
 };
 
-console.log(arr.bubbleNumber());
+// console.log(arr.bubbleNumber());
+
+// Selection
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) lowest = j;
+    }
+    [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
+    console.log("new arr", arr);
+  }
+  return arr;
+}
+
+// console.log(selectionSort([34, 22, 10, 19, 17]));
+
+// Insertion sort
+
+// function insertionSor(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     for (let j = i - 1; j >= 0; j--) {
+//       if (arr[i] < arr[j]) {
+//         [arr[i], arr[j]] = [arr[j], arr[i]];
+//         i--;
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+// enhanced code but still working on it
+function insertionSor(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+      arr[j + 1] = arr[j];
+    }
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
+
+console.log(insertionSor([34, 22, 10, 19, 17]));
