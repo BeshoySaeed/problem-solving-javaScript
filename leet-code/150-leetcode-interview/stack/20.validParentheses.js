bd2f87ff111151f67d50ef7ff902f7c8bc6ef6fa
bishoysaeed <bishoysaeed201@gmail.com>
2024-10-06 15:42:33 +0300

    valid-parentheses

---
+++ leet-code/150-leetcode-interview/stack/20.validParentheses.js
@@ -0,0 +1,17 @@
+var isValid = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else if (
+      (s[i] === ")" && stack[stack.length - 1] === "(") ||
+      (s[i] === "}" && stack[stack.length - 1] === "{") ||
+      (s[i] === "]" && stack[stack.length - 1] === "[")
+    ) {
+      stack.pop();
+    } else {
+      return false;
+    }
+  }
+  return stack.length === 0;
+};
