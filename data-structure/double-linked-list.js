b839b746046bee0e13914ba9a561d98ee881cace
Bsihoy <beshoy@nbsventure.com>
2024-10-30 22:31:32 +0300

    push_pop_shift_unshift_doubledLinkedList

--- data-structure/double-linked-list.js
+++ data-structure/double-linked-list.js
@@ -12,4 +12,71 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return newNode;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let currentTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = currentTail.prev;
+      this.tail.next = null;
+      currentTail.prev = null;
+    }
+    this.length--;
+    return currentTail;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    const currentHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    }
+    this.head = currentHead.next;
+    this.head.prev = null;
+    currentHead.next = null;
+    this.length--;
+    return currentHead;
+  }
+
+  unshift(val) {
+    const newHead = new Node(val);
+    if (!this.head) {
+      this.head = newHead;
+      this.tail = newHead;
+    } else {
+      this.head.prev = newHead;
+      newHead.next = this.head;
+      this.head = newHead;
+    }
+    this.length++;
+    return newHead;
+  }
 }
+
+const list = new DoublyLinkedList();
+
+list.push(1);
+
+list.push(2);
+
+list.pop();
+console.log(list.length);
+console.log(list);
